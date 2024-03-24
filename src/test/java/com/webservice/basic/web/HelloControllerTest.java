7dbdaf70fd012d67c9260cea591713176acc7d7d
nikeisme <nikeisme827@gmail.com>
2024-03-24 23:49:16 +0900

    HelloController 테스트 코드 작성하기

---
+++ src/test/java/com/webservice/basic/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.webservice.basic.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class) // 테스트 진행 시 JUnit에 내장된 실행자 외에 다른 실행자를 시킴, 스프링부트 테스트와 JUnit 사이에 연결자 역할
+@WebMvcTest(controllers = HelloController.class) // Web에 집중할 수 있는 어노테이션
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+}
